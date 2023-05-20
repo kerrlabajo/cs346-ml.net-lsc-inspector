ae7af1b6c8d61535ec143a5d403ab96596942a75
Raul Jay Yanson <rjyansonx@gmail.com>
2023-05-20 14:46:13 +0800

    Update

--- LSC_Intellysis/frontend/main.py
+++ LSC_Intellysis/frontend/main.py
@@ -4,7 +4,6 @@ from PIL import Image
 from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
-import torch
 
 css_path = os.path.join(os.path.dirname(__file__), "styles.css")
 absolute_css_path = os.path.abspath(css_path)
@@ -13,17 +12,15 @@ st.markdown(f"<style>{open(absolute_css_path).read()}</style>", unsafe_allow_htm
 
 st.title("LSC Quality Evaluator")
 
-# print(torch.cuda.is_available())
-# print(torch.cuda.get_device_name(0))
-
 # image_path = "../test/Test1.png"
 # img = Image.open(image_path)
 
 model = YOLO("../runs/detect/train7/weights/best.pt")
-result = model.predict(source="0", show=False, conf=0.5)
+# result = model.predict(source="../test/test6.png", show=False, conf=0.5, save=True)
+
 # print(img)
-# # Get the original image
-# res_path = "runs/detect/predict/Test1.png"
+# Get the original image
+# res_path = "runs/detect/predict6/Test6.png"
 # res = Image.open(res_path)
 
 # st.image(res)
@@ -31,65 +28,72 @@ result = model.predict(source="0", show=False, conf=0.5)
 def main():
     # Create sidebar
 
-    # Add items to the sidebar
-    history_button = st.sidebar.button(
-        "Dashboard", key="Dashboard", use_container_width=True
-    )
-    home_button = st.sidebar.button("Home", key="Home", use_container_width=True)
-    history_button = st.sidebar.button(
-        "History", key="History", use_container_width=True
-    )
-    archived_button = st.sidebar.button(
-        "Archived", key="Archived", use_container_width=True
-    )
-    settings_button = st.sidebar.button(
-        "Settings", key="Settings", use_container_width=True
-    )
+    pages = {
+      "Dashboard": render_dashboard,
+      "Home": render_home,
+      "History": render_history,
+      "Archived": render_archived,
+      "Settings": render_settings,
+    }
 
     query_params = st.experimental_get_query_params()
-    current_page = query_params.get("page", ["home"])[0]
+    current_page = query_params.get("page", ["Home"])[0]
 
-    # Render sidebar items
-    if home_button:
-        query_params["page"] = "home"
+    for page_name, page_func in pages.items():
+      button = st.sidebar.button(page_name, key=page_name, use_container_width=True)
+      if button:
+        query_params["page"] = page_name
         st.experimental_set_query_params(**query_params)
-        render_home()
-
-    elif history_button:
-        query_params["page"] = "history"
-        st.experimental_set_query_params(**query_params)
-        render_history()
-
-    elif archived_button:
-        query_params["page"] = "archived"
-        st.experimental_set_query_params(**query_params)
-        render_archived()
-
-    elif settings_button:
-        query_params["page"] = "settings"
-        st.experimental_set_query_params(**query_params)
+        page_func()
 
 
-# Image upload button
-uploaded_file = st.file_uploader("Upload an image")
-print(uploaded_file)
-# Process the uploaded image if available
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.subheader("Uploaded Image")
+    # Image upload button
+    uploaded_file = st.file_uploader("Upload an image")
+    print(uploaded_file)
+    # Process the uploaded image if available
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        print(uploaded_file)
+        st.subheader("Uploaded Image")
+        image_width = 500
+        aspect_ratio = image.width / image.height
+        image_height = int(image_width / aspect_ratio)
+        st.image(image, width=image_width)
+        analyze_button = st.button("Analyze")
+        if analyze_button:
+            analyze_image(uploaded_file)
+
+        # result = model.predict(source=image, show=False, conf=0.5, save=True)
+
+        # # Get the original image
+        # res_path = "runs/detect/predict/Test1.png"
+        # res = Image.open(res_path)
+
+        # st.image(res)
+
+
+def analyze_image(file):
+    res_img = Image.open(file)
+    print("Image here: ")
+    print(res_img.info)
+    st.subheader("Analyzed Image")
     image_width = 500
-    aspect_ratio = image.width / image.height
+    aspect_ratio = res_img.width / res_img.height
     image_height = int(image_width / aspect_ratio)
-    st.image(image, width=image_width)
-  
+    # st.image(res_img, width=image_width)  
 
-    # result = model.predict(source=image, show=False, conf=0.5, save=True)
-
-    # # Get the original image
-    # res_path = "runs/detect/predict/Test1.png"
+    result = model.predict(source=res_img.filename + res_img.format, show=False, conf=0.5, save=True)
+    # Get the original image
+    # res_path = "runs/detect/predict6/Test6.png"
     # res = Image.open(res_path)
 
+    # st.subheader("Analyzed Image")
     # st.image(res)
+
+
+def render_dashboard():
+    st.title("Dashboard")
+    st.write("Welcome to the Dashboard page.")
 
 
 def render_home():
